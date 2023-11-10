f5a7404064a06b71ba32fbec0517801745fa9d23
TalokanFarmer <truongthehien29062002@gmail.com>
2023-11-10 15:41:57 +0700

    update img

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,10 @@ function App() {
       <div className="container">
         <div className="banner">
           <div className="prf_bgr">
-            <img src="https://img4.thuthuatphanmem.vn/uploads/2020/07/05/background-cong-nghe-full-hd_035953614.jpg" alt />
+            <img src="https://img4.thuthuatphanmem.vn/uploads/2020/07/05/background-cong-nghe-full-hd_035953614.jpg" alt="" />
           </div>
           <div className="prf_img">
-            <img src="https://scontent.fhan15-1.fna.fbcdn.net/v/t39.30808-6/387741847_1005671027318575_1815665505677506037_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=HN-Tb62c9FoAX-3A8ky&_nc_ht=scontent.fhan15-1.fna&oh=00_AfBL2gzhiKl79wC1dUYR3yCTt1_G4gMQq4a4oCkTdm6GWw&oe=655261E7" alt />
+            <img src="../public/img/387741847_1005671027318575_1815665505677506037_n.jpg" alt="" />
           </div>
         </div>
         <div className="content">

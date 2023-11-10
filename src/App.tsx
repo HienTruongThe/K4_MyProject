275f09d4e8de414831d3b031216bf492414d2e28
TalokanFarmer <truongthehien29062002@gmail.com>
2023-11-10 15:39:36 +0700

    day01

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="container">
+        <div className="banner">
+          <div className="prf_bgr">
+            <img src="https://img4.thuthuatphanmem.vn/uploads/2020/07/05/background-cong-nghe-full-hd_035953614.jpg" alt />
+          </div>
+          <div className="prf_img">
+            <img src="https://scontent.fhan15-1.fna.fbcdn.net/v/t39.30808-6/387741847_1005671027318575_1815665505677506037_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_ohc=HN-Tb62c9FoAX-3A8ky&_nc_ht=scontent.fhan15-1.fna&oh=00_AfBL2gzhiKl79wC1dUYR3yCTt1_G4gMQq4a4oCkTdm6GWw&oe=655261E7" alt />
+          </div>
+        </div>
+        <div className="content">
+          <p>Họ tên: Trương Thế Hiền</p>
+          <p>Ngày sinh: 29/06/2002</p>
+          <p>Quê quán: Thanh Hoá</p>
+          <p>Định hướng: FullStack Web Developer</p>
+          <p>Ngôn ngữ: C, Java, JavaScript, PHP</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
